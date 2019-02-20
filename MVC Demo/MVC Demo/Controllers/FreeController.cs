d7df8bee1fdf0be53c1f43787e8255113ea5b3cf
Matt Petermann <MPetermann2@my.wctc.edu>
2019-02-19 20:20:29 -0600

    Add MVC Demo

---
+++ MVC Demo/MVC Demo/Controllers/FreeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class FreeController : Controller
+    {
+        // GET: Free
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bird(string name, int? age, string color)
+        {
+            ViewBag.Name = name ?? "Unknown";
+            ViewBag.Age = age ?? -1;
+            ViewBag.Color = color  ?? "N/A";
+            return View();
+        }
+    }
+}
