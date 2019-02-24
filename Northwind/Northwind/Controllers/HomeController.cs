c5f476c6905fbc2e1f1ab5065912ec261ed8fde8
MATTPC\Matt <matthewp214@gmail.com>
2019-02-24 00:00:18 -0600

    Add northwind

---
+++ Northwind/Northwind/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Your product page.";
+
+            return View();
+        }
+
+        public ActionResult Account()
+        {
+            ViewBag.Message = "Your account page.";
+
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Your cart page.";
+
+            return View();
+        }
+    }
+}
