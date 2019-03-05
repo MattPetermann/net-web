a6f698a551b5c2bafd3f01040cc3a0eb1fa8810f
MATTPC\Matt <matthewp214@gmail.com>
2019-03-04 23:50:43 -0600

    Add Balloons project

---
+++ Balloons/Balloons/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Balloons.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult ViewCart(FormCollection collection)
+        {
+            string[] keys = collection.AllKeys;
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            
+            foreach(string key in keys)
+            {
+                keyValuePairs.Add(key, collection[key]);
+            }
+
+            ViewBag.FormResults = keyValuePairs;
+
+            return View();
+        }
+    }
+}
---
+++ Balloons/Balloons/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Balloons.MvcApplication" Language="C#" %>
