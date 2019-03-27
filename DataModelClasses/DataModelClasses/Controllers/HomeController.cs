fcce905874d91587dbccb7f0aee2ee5d0b9af339
Matt Petermann <MPetermann2@my.wctc.edu>
2019-03-26 19:05:14 -0500

    data model example

---
+++ DataModelClasses/DataModelClasses/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataModelClasses.Models;
+
+namespace DataModelClasses.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Products()
+		{
+			return View(ProductContext.GetProducts());
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ DataModelClasses/DataModelClasses/Models/ProductContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataModelClasses.Models
+{
+	public class ProductContext
+	{
+		List<Product> _products;
+
+		public ProductContext()
+		{
+			_products = new List<Product>()
+			{
+				new Product { Id=100, Name="CD", Price=10.00m},
+				new Product { Id=101, Name="Printer", Price=200.00m},
+				new Product { Id=102, Name="Stuffed Animal", Price=5.00m}
+			};
+		}
+
+		public List<Product> GetProducts()
+		{
+			return _products;
+		}
+
+		public Product Find(int id) => _products.FirstOrDefault(p => p.Id == id);
+	}
+}
